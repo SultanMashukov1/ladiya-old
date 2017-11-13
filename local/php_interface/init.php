a7fa87826ef92abe20d232a4b818f404cf35f798
kopoji <kopojiq@gmail.com>
2017-11-13 16:11:51 +0300

    replace init.php to local

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -7,4 +7,4 @@ function includeArea($file, $withSiteDir = true)
 
 defined('_DS_') or define('_DS_', DIRECTORY_SEPARATOR);
 if(is_file($_SERVER['DOCUMENT_ROOT'] . _DS_ . 'local' . _DS_ . 'WM' . _DS_ . 'autoloader.php'))
-    require_once $_SERVER['DOCUMENT_ROOT'] . _DS_ . 'local' . _DS_ . 'WM' . _DS_ . 'autoloader.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . _DS_ . 'local' . _DS_ . 'WM' . _DS_ .  'autoloader.php';
