e15518f3bcc14e90e0c745d3f9c90fd675c21c43
kopoji <za9bali_so_vzlomom>
2017-11-13 15:32:48 +0300

    update lib (add regex validator)

---
+++ local/WM/lib/Forms/Validators/RegexValidator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace WM\Forms\Validators;
+
+
+/**
+ * Class RegexValidator
+ * @package WM\Forms\Validators
+ */
+class RegexValidator extends Validator
+{
+    /**
+     *
+     */
+    const DEFAULT_MESSAGE = 'Неверный формат {name}';
+
+    /**
+     * Check if field is valid by regex pattern (PCRE format)
+     *
+     * @return bool
+     *
+     * @access public
+     */
+    public function validate()
+    {
+        return !($this->hasError = !preg_match($this->additionalParams['pattern'], $this->checkValue));
+    }
+
+    /**
+     * @param string $value
+     * @return void
+     */
+    protected function setDataPattern($value)
+    {
+        $this->additionalParams['pattern'] = $value;
+    }
+}
