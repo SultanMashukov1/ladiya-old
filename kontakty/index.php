e07112df215e378e64df139228cec5bec665056f
LeMa <kopojiq@gmail.com>
2017-11-15 11:32:12 +0300

    change

--- kontakty/index.php
+++ kontakty/index.php
@@ -14,7 +14,34 @@ $APPLICATION->SetTitle("Контакты");
     </div>
     <div class="container">
         <div class="text__block">
-            <p>Текст</p>
+            <h2>ООО Туристская компания «Ладья»</h2>
+            <div class="text__block__table">
+                <div class="text__block__table__cell">
+                    <p>
+                        <b>Наш адрес:</b><br>
+                        357500, Ставропольский край, г. Пятигорск, проспект Кирова, 90 (район ж/д вокзала)<br>
+                        <br>
+                        <b>Контакты:</b><br>
+                        тел. (8793) 39-44-03, 39-17-17<br>
+                        факс (8793) 39-44-03<br>
+                        <br>
+                        сот:+7 (905) 464-90-80<br>
+                        +7 (938) 345-90-00<br>
+                        <br>
+                        <b>Электронный адрес:</b><br>
+                        info@ladya-kmv.ru<br>
+                        <br>
+                        <b>ICQ-Клиент:</b><br>
+                        426932902<br>
+                        <br>
+                        <b>График работы:</b><br>
+                        пон-суб с 10-00 до 18-00, вс - выходной<br>
+                    </p>
+                </div>
+                <div class="text__block__table__cell">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2868.3016253900714!2d43.05549341565294!3d44.035824379110046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x405776320218a073%3A0xef9ab18d1ce895c4!2z0JrQuNGA0L7QstCwINC_0YDQvtGB0L8uLCA5MCwg0J_Rj9GC0LjQs9C-0YDRgdC6LCDQodGC0LDQstGA0L7Qv9C-0LvRjNGB0LrQuNC5INC60YDQsNC5LCAzNTc1MDI!5e0!3m2!1sru!2sru!4v1510669320729" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+            </div>
         </div>
     </div>
 </div>
