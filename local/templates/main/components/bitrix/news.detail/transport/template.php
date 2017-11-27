92e9bee60288808daad989dfeb85992c579fb988
DoNotEvenThink <alekseivasin1993@gmail.com>
2017-11-27 18:09:51 +0300

    add template bitrix.news.detail

---
+++ local/templates/main/components/bitrix/news.detail/transport/template.php
@@ -0,0 +1,33 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="news-detail">
+    <div class="container">
+        <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arResult["DETAIL_PICTURE"])): ?>
+            <img
+                    class="detail_picture"
+                    border="0"
+                    src="<?= $arResult["DETAIL_PICTURE"]["SRC"] ?>"
+                    width="<?= $arResult["DETAIL_PICTURE"]["WIDTH"] ?>"
+                    height="<?= $arResult["DETAIL_PICTURE"]["HEIGHT"] ?>"
+                    alt="<?= $arResult["DETAIL_PICTURE"]["ALT"] ?>"
+                    title="<?= $arResult["DETAIL_PICTURE"]["TITLE"] ?>"
+            />
+        <? endif ?>
+        <? if ($arResult["DETAIL_TEXT"]): ?>
+            <? echo $arResult["DETAIL_TEXT"]; ?>
+        <? endif ?>
+
+    </div>
+</div>
