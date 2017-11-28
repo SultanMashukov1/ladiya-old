78cd9e3a59649012309422a7beefade617525b24
DoNotEvenThink <alekseivasin1993@gmail.com>
2017-11-28 15:36:38 +0300

    integrated page contacts

--- kontakty/index.php
+++ kontakty/index.php
@@ -1,48 +1,75 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Контакты");
 ?>
-<div class="text__block__wrap">
-    <div class="text___block__images" style="background-image: url('/assets/img/carousel/5.png')">
-        <div class="container">
-            <span class="text___block__images__title">Контакты</span>
-            <ul class="text___block__breadcrumbs">
-                <li><a href="/">Главная</a></li>
-                <li><span>Новости</span></li>
-            </ul>
+    <div class="text__block__wrap">
+        <div class="text___block__images" style="background-image: url('/assets/img/carousel/5.png')">
+            <div class="container">
+                <span class="text___block__images__title"><?= $APPLICATION->ShowTitle(); ?></span>
+                <? $APPLICATION->IncludeComponent(
+                    "bitrix:breadcrumb",
+                    "company",
+                    Array(
+                        "PATH" => "",    // Путь, для которого будет построена навигационная цепочка (по умолчанию, текущий путь)
+                        "SITE_ID" => "s1",    // Cайт (устанавливается в случае многосайтовой версии, когда DOCUMENT_ROOT у сайтов разный)
+                        "START_FROM" => "0",    // Номер пункта, начиная с которого будет построена навигационная цепочка
+                    ),
+                    false
+                ); ?>
+            </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="text__block">
-            <h2>ООО Туристская компания «Ладья»</h2>
-            <div class="text__block__table">
-                <div class="text__block__table__cell">
-                    <p>
-                        <b>Наш адрес:</b><br>
-                        357500, Ставропольский край, г. Пятигорск, проспект Кирова, 90 (район ж/д вокзала)<br>
-                        <br>
-                        <b>Контакты:</b><br>
-                        тел. (8793) 39-44-03, 39-17-17<br>
-                        факс (8793) 39-44-03<br>
-                        <br>
-                        сот:+7 (905) 464-90-80<br>
-                        +7 (938) 345-90-00<br>
-                        <br>
-                        <b>Электронный адрес:</b><br>
-                        info@ladya-kmv.ru<br>
-                        <br>
-                        <b>ICQ-Клиент:</b><br>
-                        426932902<br>
-                        <br>
-                        <b>График работы:</b><br>
-                        пон-суб с 10-00 до 18-00, вс - выходной<br>
-                    </p>
-                </div>
-                <div class="text__block__table__cell">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2868.3016253900714!2d43.05549341565294!3d44.035824379110046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x405776320218a073%3A0xef9ab18d1ce895c4!2z0JrQuNGA0L7QstCwINC_0YDQvtGB0L8uLCA5MCwg0J_Rj9GC0LjQs9C-0YDRgdC6LCDQodGC0LDQstGA0L7Qv9C-0LvRjNGB0LrQuNC5INC60YDQsNC5LCAzNTc1MDI!5e0!3m2!1sru!2sru!4v1510669320729" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <div class="container">
+            <div class="text__block">
+                <h2><? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/TITLE.php')); ?></h2>
+                <div class="text__block__table">
+                    <div class="text__block__table__cell">
+                        <p>
+                            <b><? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/address_title.php')); ?></b><br>
+                            <? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/address_description.php')); ?>
+                            <br>
+                            <b><? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/contacts_title.php')); ?></b><br>
+                            <? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/contacts_description.php')); ?>
+                            <br>
+                            <b><? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/email_title.php')); ?></b><br>
+                            <? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/email_description.php')); ?>
+                            <br>
+                            <b><? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/icq_title.php')); ?></b><br>
+                            <? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/icq_description.php')); ?>
+                            <br>
+                            <b><? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/schedule_title.php')); ?></b><br>
+                            <? \WM\Components\IncludeArea::inc('', array('PATH' => SITE_DIR . 'include/contacts/schedule_description.php')); ?>
+                            <br>
+                        </p>
+                    </div>
+                    <div class="text__block__table__cell">
+                        <? $APPLICATION->IncludeComponent(
+                            "bitrix:map.google.view",
+                            ".default",
+                            array(
+                                "API_KEY" => "AIzaSyDQmNMDanuTYUgnqJhgVu4880h2qh1N1Qw",
+                                "CONTROLS" => array(
+                                    0 => "SMALL_ZOOM_CONTROL",
+                                    1 => "TYPECONTROL",
+                                    2 => "SCALELINE",
+                                ),
+                                "INIT_MAP_TYPE" => "ROADMAP",
+                                "MAP_DATA" => "a:4:{s:10:\"google_lat\";d:44.0358712780855;s:10:\"google_lon\";d:43.05768269999995;s:12:\"google_scale\";i:17;s:10:\"PLACEMARKS\";a:1:{i:0;a:3:{s:4:\"TEXT\";s:95:\"Кирова просп., 90, Пятигорск, Ставропольский край, 357502\";s:3:\"LON\";d:43.05762737989426;s:3:\"LAT\";d:44.03578003459076;}}}",
+                                "MAP_HEIGHT" => "450",
+                                "MAP_ID" => "",
+                                "MAP_WIDTH" => "600",
+                                "OPTIONS" => array(
+                                    0 => "ENABLE_SCROLL_ZOOM",
+                                    1 => "ENABLE_DBLCLICK_ZOOM",
+                                    2 => "ENABLE_DRAGGING",
+                                    3 => "ENABLE_KEYBOARD",
+                                ),
+                                "COMPONENT_TEMPLATE" => ".default"
+                            ),
+                            false
+                        ); ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
