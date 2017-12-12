2706c76379b0e30d0b8c4672d48267fdcada1a5d
DoNotEvenThink <alekseivasin1993@gmail.com>
2017-12-12 18:17:50 +0300

    add page photo-gallery

---
+++ photo/detail.php
@@ -0,0 +1,83 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+?>
+    <div class="text__block__wrap">
+        <div class="text___block__images" style="background-image: url('/assets/img/carousel/5.png')">
+            <div class="container">
+                <span class="text___block__images__title"><?= $APPLICATION->ShowTitle(); ?></span>
+                <? $APPLICATION->IncludeComponent(
+                    "bitrix:breadcrumb",
+                    "company",
+                    Array(
+                        "PATH" => "",
+                        "SITE_ID" => "s1",
+                        "START_FROM" => "0"
+                    )
+                ); ?>
+            </div>
+        </div>
+        <? $APPLICATION->IncludeComponent(
+            "bitrix:news.detail",
+            "photo",
+            array(
+                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                "ADD_ELEMENT_CHAIN" => "Y",
+                "ADD_SECTIONS_CHAIN" => "N",
+                "AJAX_MODE" => "N",
+                "AJAX_OPTION_ADDITIONAL" => "",
+                "AJAX_OPTION_HISTORY" => "N",
+                "AJAX_OPTION_JUMP" => "N",
+                "AJAX_OPTION_STYLE" => "Y",
+                "BROWSER_TITLE" => "-",
+                "CACHE_GROUPS" => "Y",
+                "CACHE_TIME" => "36000000",
+                "CACHE_TYPE" => "A",
+                "CHECK_DATES" => "Y",
+                "DETAIL_URL" => "",
+                "DISPLAY_BOTTOM_PAGER" => "N",
+                "DISPLAY_DATE" => "Y",
+                "DISPLAY_NAME" => "Y",
+                "DISPLAY_PICTURE" => "Y",
+                "DISPLAY_PREVIEW_TEXT" => "Y",
+                "DISPLAY_TOP_PAGER" => "N",
+                "ELEMENT_CODE" => $_REQUEST["CODE"],
+                "ELEMENT_ID" => "",
+                "FIELD_CODE" => array(
+                    0 => "CODE",
+                    1 => "DETAIL_TEXT",
+                    2 => "DETAIL_PICTURE",
+                    3 => "",
+                ),
+                "IBLOCK_ID" => "17",
+                "IBLOCK_TYPE" => "content",
+                "IBLOCK_URL" => "",
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                "MESSAGE_404" => "",
+                "META_DESCRIPTION" => "-",
+                "META_KEYWORDS" => "-",
+                "PAGER_BASE_LINK_ENABLE" => "N",
+                "PAGER_SHOW_ALL" => "N",
+                "PAGER_TEMPLATE" => ".default",
+                "PAGER_TITLE" => "Страница",
+                "PROPERTY_CODE" => array(
+                    0 => "",
+                    1 => "MORE_PHOTO",
+                    2 => "",
+                ),
+                "SET_BROWSER_TITLE" => "N",
+                "SET_CANONICAL_URL" => "N",
+                "SET_LAST_MODIFIED" => "N",
+                "SET_META_DESCRIPTION" => "N",
+                "SET_META_KEYWORDS" => "N",
+                "SET_STATUS_404" => "N",
+                "SET_TITLE" => "Y",
+                "SHOW_404" => "N",
+                "STRICT_SECTION_CHECK" => "N",
+                "USE_PERMISSIONS" => "N",
+                "USE_SHARE" => "N",
+                "COMPONENT_TEMPLATE" => "photo"
+            ),
+            false
+        ); ?>
+    </div>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
