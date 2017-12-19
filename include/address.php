6654950ed1fb1f54199276341e21ee56a096a9b2
SultanMashukov1 <mashukov.s@lema.agency>
2017-12-19 16:36:59 +0300

    test

--- include/address.php
+++ include/address.php
@@ -1,3 +1,5 @@
-<span style="color: #ffffff;">357500, Ставропольский край, г. Пятигорск, проспект&nbsp;Кирова,&nbsp;90</span><br>
-<span style="color: #ffffff;">Телефон: 8 (800) 333-99-21, +7 (8793) 39-44-03</span><br>
-<span style="color: #ffffff;">Телефон для экстренной связи: +7 (938) 35-15-000</span>
+<p style="text-align: left;">
+	<span style="color: #ffffff;">357500, Ставропольский край, г. Пятигорск, проспект&nbsp;Кирова,&nbsp;90</span><br>
+ <span style="color: #ffffff;">Телефон: 8 (800) 333-99-21, +7 (8793) 39-44-03</span><br>
+ <span style="color: #ffffff;">Телефон для экстренной связи: +7 (938) 35-15-000</span>
+</p>
