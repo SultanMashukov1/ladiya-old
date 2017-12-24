3714c1061beca79f0cdd2365d943afa1a2837fe6
SultanMashukov1 <mashukov.s@lema.agency>
2017-12-25 00:39:51 +0300

    Add form on "rent-bus" page

--- rent-bus/index.php
+++ rent-bus/index.php
@@ -60,7 +60,7 @@ $APPLICATION->SetTitle("Аренда автобусов");
                             </div>
                             <div class="form__filter__input  it-block">
                                 <select name="select_from" class="form__filter__select__control cs-select cs-skin-border">
-                                    <option value="" selected="selected">Выбрать</option>
+                                    <option value="" >Выбрать</option>
                                     <option value="2">Белокуриха</option>
                                     <option value="3">Бийск</option>
                                     <option value="4">Новоалтайск</option>
@@ -76,7 +76,7 @@ $APPLICATION->SetTitle("Аренда автобусов");
                             </div>
                             <div class="form__filter__input  it-block">
                                 <select name="select_to" class="form__filter__select__control cs-select cs-skin-border">
-                                    <option value="" selected="selected">Выбрать</option>
+                                    <option value="" >Выбрать</option>
                                     <option value="2">Белокуриха</option>
                                     <option value="3">Бийск</option>
                                     <option value="4">Новоалтайск</option>
