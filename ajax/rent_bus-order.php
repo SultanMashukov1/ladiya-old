692e09000b4a5a7a7885a6c42dfb837887bdb605
SultanMashukov1 <mashukov.s@lema.agency>
2017-12-25 00:16:08 +0300

    Add form on "rent-bus" page

--- ajax/rent_bus-order.php
+++ ajax/rent_bus-order.php
@@ -12,7 +12,7 @@ $form = new \WM\Forms\AjaxForm(array(
     array('name', 'regex', array('pattern' => '~^[А-я Ё]+$~iu', 'message' => 'Недопустимые значения')),
     array('phone', 'phone', array('message' => 'Телефон должен быть в формате +7 (999) 666-33-11')),
     array('email', 'email'),
-    array('date', 'required',array('message' => 'Выберите дату встречи')),
+    array('date-arrive', 'required',array('message' => 'Выберите дату встречи')),
 
 ),
     $_POST
