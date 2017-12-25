fec68686c83ed8f3ca41040d245c796b6ce4b36e
SultanMashukov1 <mashukov.s@lema.agency>
2017-12-25 15:16:31 +0300

    transfer was modified

--- ajax/transfer-order.php
+++ ajax/transfer-order.php
@@ -13,7 +13,7 @@ $form = new \WM\Forms\AjaxForm(array(
     array('phone', 'phone', array('message' => 'Телефон должен быть в формате +7 (999) 666-33-11')),
     array('email', 'email'),
     array('date-arrive', 'required',array('message' => 'Выберите дату встречи')),
-    array('date-arrive', 'regex', array('pattern' => '/([0-2]\d|3[01])\.(0\d|1[012])\.(\d{4})/', 'message' => 'Неправильный формат даты')),
+    array('date-arrive', 'regex', array('pattern' => '/^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$/', 'message' => 'Неправильный формат даты')),
     array('select-from', 'required',array('message' => 'Выберите место встречи')),
     array('select-to', 'required',array('message' => 'Выберите место назначения')),
 
